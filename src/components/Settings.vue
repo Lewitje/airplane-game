d00a19f2cb6cf5cc3992aa92d2adc41685c0411c
Lewi <contact@lewihussey.com>
2019-10-07 20:20:48 +0200

    Tweaks starting config

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -53,7 +53,7 @@ export default {
   },
   data () {
     return {
-      showMenu: true
+      showMenu: false
     }
   },
   created () {
--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -66,7 +66,7 @@ export default {
   },
   data () {
     return {
-      visible: false,
+      visible: true,
       step: 1
     }
   },
